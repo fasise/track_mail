8d411dc1353a265d2116fc7d809abb89cc46f921
fasise <114915933+fasise@users.noreply.github.com>
2023-09-19 01:49:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, send_file
 
 app = Flask(__name__)
 
-@app.route('/im.png')
+@app.route('/')
 def serve_pixel():
     # Lire l'image depuis le disque et la servir
-    return send_file('im.png', mimetype='im/png')
+    return send_file('im.png', mimetype='image/png')
 
 if __name__ == '__main__':
          app.run()
