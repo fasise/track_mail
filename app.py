6de0ebf1579c3dbcf80dc57b23dad8206e40c78c
fasise <114915933+fasise@users.noreply.github.com>
2023-09-19 01:19:31 +0200

    Update app.py
    pixel to im

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, send_file
 
 app = Flask(__name__)
 
-@app.route('/pixel.png')
+@app.route('/im.png')
 def serve_pixel():
     # Lire l'image depuis le disque et la servir
-    return send_file('pixel.png', mimetype='image/png')
+    return send_file('im.png', mimetype='im/png')
 
 if __name__ == '__main__':
          app.run()
